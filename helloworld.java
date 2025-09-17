6efb78e92c0a98de38704434acf5a284b72c6b05
Amar1006 <amardeepm1006@gmail.com>
2025-09-17 10:55:11 +0530

    Update helloworld.java

--- helloworld.java
+++ helloworld.java
@@ -4,6 +4,7 @@ class HelloWorld
     {
         System.out.println("Hello, World! I want to become devops engineer.");
         System.out.println("Hello, World! My jenkin server is running.");
-        System.out.println("Hello, World! This line added with the help of jenkinsdev editor");       
+        System.out.println("Hello, World! This line added with the help of jenkinsdev editor"); 
+        System.out.println("Hello, World! This line added with the help of jenkinsdevloper editor");
     }
 }
